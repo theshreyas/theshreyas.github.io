10c3b0b17fed33fc03da74e29344fcb7fd292351
theshreyas <74211645+theshreyas@users.noreply.github.com>
2022-03-17 21:35:00 +0530

    misc improvements
    title,seo,average multiplication display and other improvements

--- assets/script.js
+++ assets/script.js
@@ -15,48 +15,57 @@
 		});
 	};
 }(jQuery));
+
 $(document).ready(function () {
-	$("input").inputFilter(function (value) { //only digits & dots accepted
-		return /\d*\.?\d*/.test(value);
+	$("input").inputFilter(function (value) {
+		return /\d*\.?\d*/.test(value); //only digits & dots accepted
 	});
+	$('#cmp').val(localStorage.getItem("cmp"));
+	$('#qty').val(localStorage.getItem("qty"));
+	$('#avg').val(localStorage.getItem("avg"));
+
 	DynamicInputWidthChange();
 	$('body').on('click', 'li', function (e) {
 		if (!$(e.currentTarget).hasClass('active')) {
-			$('li').toggleClass('active');
-			$('.selltype').toggleClass('active');
+			$('li,.selltype').toggleClass('active');
 		}
 	});
 });
 $(".goback").click(function () {
 	$(".inputForm").show();
-	$(".result").hide();
-	$(".nav").hide();
-	$(".goback").hide();
+	$(".result,.nav,.goback").hide();
 });
 $("#submit").click(function (event) {
 	let cmp = parseInt($('#cmp').val()) || 0;
 	let qty = parseInt($('#qty').val()) || 0;
 	let avg = parseInt($('#avg').val()) || 0;
-  $('.inputForm input').each(function () {
-    if(!parseInt($(this).val()))
-      $(this).css('border-color', 'red');
-    else
-      $(this).css('border-color', 'black');
+
+	$('.inputForm input').each(function () {
+		if(!parseInt($(this).val()))
+		$(this).css('border-color', 'red');
+		else
+		$(this).css('border-color', 'black');
 	});
+	
 	if (cmp == 0 || qty == 0 || avg == 0) {
 		return false;
 	}
+    localStorage.setItem("cmp",cmp);
+    localStorage.setItem("qty",qty);
+    localStorage.setItem("avg",avg);
+	
 	$(".inputForm").hide();
-	$(".goback").show();
-	$(".result").show();
+	$(".goback,.result").show();
 	$(".result:first").attr('style', 'display: flex !important;justify-content: center;margin-top: 10px;');
 	$(".nav").attr('style', 'display: flex !important;');
 	$(".result.text-center").attr('style', 'display: block !important;');
 	$('#netgain').val(0);
+
 	let totalinvestment = qty * avg;
 	let profit = 0;
 	let profit_per = 0;
 	let currentValue = cmp * qty;
+
 	profit = currentValue - (totalinvestment);
 	profit_per = profit / totalinvestment * 100;
 	profit = Math.abs(profit).toLocaleString('en-IN', {
@@ -95,8 +104,9 @@ $("#submit").click(function (event) {
 		$('#netg,#sellp').css('color', 'red');
 	}
 	let selltotalqty = qty + buyqty;
-	let sellprice = (totalinvestment + buyprice * buyqty) / selltotalqty;
-
+	let finalinvestment = totalinvestment + buyprice * buyqty;
+	let sellprice = finalinvestment / selltotalqty;
+	let sellprofit =  Math.round(totalinvestment * netg / 100);
 	totalinvestment = totalinvestment.toLocaleString('en-IN', {
 		maximumFractionDigits: 0,
 		style: 'currency',
@@ -104,9 +114,16 @@ $("#submit").click(function (event) {
 	});
 	$('#invested').html(totalinvestment);
 	$('#curvalue').html(currentValue);
-
-	$('#buyqty').val(Math.round(buyqty));
-	$('#buyprice').val(Math.round(buyprice));
+	buyqty = Math.round(buyqty);
+	buyprice = Math.round(buyprice);
+	$('#buyqty').val(buyqty);
+	$('.footertext .buyqty').html(' '+buyqty);
+	$('.footertext .buyprice').html(' '+buyprice);
+	$('.footertext .addtotal').html(' '+buyprice*buyqty);
+	$('.footertext .totalinvested').html(' '+ Math.round(finalinvestment));
+	$('.footertext .profit').html(' 0');
+	$('.footertext .sell-profit').html(' '+sellprofit);
+	$('#buyprice').val(buyprice);
 	$('#sellprice').val(Math.round(sellprice));
 	$('#sellp').val(sellp);
 	$('#netg').val(netg);
@@ -131,7 +148,8 @@ $('body').on('input', '#buyqty,#buyprice,#netgain', function () {
 	var sellprice = parseInt($('#sellprice').val());
 	var netgain = $("#netgain").val();
 	var selltotalqty = qty + buyqty;
-	var sellprice = (qty * avg + buyprice * buyqty) / selltotalqty;
+	var totalinvestment = (qty * avg + buyprice * buyqty); 
+	var sellprice = totalinvestment / selltotalqty;
 	sellprice = Math.round(sellprice + sellprice * netgain / 100);
 	if (netgain > 0) {
 		$('#netgain,#sellprice').css('color', 'green');
@@ -140,6 +158,11 @@ $('body').on('input', '#buyqty,#buyprice,#netgain', function () {
 		$('#netgain,#sellprice').css('color', 'red');
 	}
 	$('#sellprice').val(sellprice);
+	$('.footertext .buyqty').html(' '+buyqty);
+	$('.footertext .buyprice').html(' '+buyprice);
+	$('.footertext .addtotal').html(' '+buyprice*buyqty);
+	$('.footertext .totalinvested').html(' '+ Math.round(totalinvestment));
+	$('.footertext .profit').html(' '+ Math.round(totalinvestment*netgain/100));
 	DynamicInputWidthChange();
 
 });
@@ -156,6 +179,7 @@ $('body').on('input', '#sellp', function () {
 		$('#netg,#sellp').css('color', 'red');
 	}
 	$('#netg').val(netgain);
+	$('.footertext .sell-profit').html(' '+ Math.round(qty*avg*netgain/100));
 	DynamicInputWidthChange();
 });
 
@@ -168,6 +192,8 @@ $('body').on('input', '#sellprice', function () {
 
 	var selltotalqty = qty + buyqty;
 	var buyprice = (sellprice * (qty + buyqty) / (1 + netgain / 100) - (qty * avg)) / buyqty;
+	var totalinvested = qty*avg + buyprice*buyqty;
+	var profit = totalinvested * netgain / 100;
 	if (buyprice < 0) {
 		$('#sellprice').css('color', 'red');
 		$('#sellprice').effect('shake', {
@@ -178,20 +204,27 @@ $('body').on('input', '#sellprice', function () {
 		});
 		return;
 	}
+	buyprice = Math.round(buyprice);
 	//  $(this).width($(this).val().length * 48);
-	$('#buyprice').val(Math.round(buyprice));
+	$('#buyprice').val(buyprice);
+	$('.footertext .buyqty').html(' '+buyqty);
+	$('.footertext .buyprice').html(' '+buyprice);
+	$('.footertext .addtotal').html(' '+buyprice*buyqty);
+	$('.footertext .totalinvested').html(' '+ Math.round(totalinvested));
+	$('.footertext .profit').html(' '+ Math.round(profit));
 	DynamicInputWidthChange();
 });
 $('body').on('click', '.close', function () {
-	$('#demo-modal').hide();
+	$('#welldone').hide();
 });
 $('body').on('click', 'a', function () {
-	$('#demo-modal').show();
+	$('#welldone').show();
 });
 
 
 $('body').on('input', '#netg', function () {
 	var avg = parseInt($('#avg').val());
+	var qty = parseInt($('#qty').val());
 	var netgain = parseInt($('#netg').val());
 
 	var sellprice = Math.round(avg + avg * netgain / 100);
@@ -202,17 +235,18 @@ $('body').on('input', '#netg', function () {
 		$('#netg,#sellp').css('color', 'red');
 	}
 	$('#sellp').val(sellprice);
+	$('.footertext .sell-profit').html(' '+ Math.round(qty*avg*netgain/100));
 	//  $(this).width($(this).val().length * 48);
 	DynamicInputWidthChange();
 
 });
-// let ct = localStorage.getItem("visited3");
-// if(!ct){
-//     localStorage.setItem("visited3",parseInt(1));
-// }
-// else{
-//    if(ct >= 3){
-//       $('.first').hide();
-//    }
-//    localStorage.setItem("visited3",parseInt(ct)+1);
-// }
+let ct = localStorage.getItem("visited");
+if(!ct || ct == 'NaN'){
+    localStorage.setItem("visited",parseInt(1));
+}
+else{
+   if(ct >= 3){
+      $('.first').hide();
+   }
+   localStorage.setItem("visited",parseInt(ct)+1);
+}
