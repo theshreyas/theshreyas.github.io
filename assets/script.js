cf93c215ccd2b98a54ae1a27b36dffe9976ec2b3
theshreyas <74211645+theshreyas@users.noreply.github.com>
2022-07-03 01:36:14 +0530

    design changes

--- assets/script.js
+++ assets/script.js
@@ -30,7 +30,7 @@ $(document).ready(function() {
     });
 });
 $(".goback").click(function() {
-    $(".inputForm").show();
+    $(".inputForm,.otherbuttons").show();
     $(".result,.nav,.goback").hide();
 });
 $('body').on('click', '#averageCalculator', function(e) {
@@ -41,11 +41,11 @@ $('body').on('click', '#averageCalculator', function(e) {
     // console.log(id);
 });
 $('body').on('click', '#percentageCalculator', function() {
-    $('.lossCalculator,.averageCalculator,#percentageCalculator').hide();
+    $('.lossCalculator,.averageCalculator,#percentageCalculator,.averageResult').hide();
     $('.percentageCalculator,#averageCalculator,#lossCalculator').show();
 });
 $('body').on('click', '#lossCalculator', function() {
-    $('.percentageCalculator,.averageCalculator,#lossCalculator').hide();
+    $('.percentageCalculator,.averageCalculator,#lossCalculator,.averageResult').hide();
     $('.lossCalculator,#averageCalculator,#percentageCalculator').show();
 });
 $(document).on('input', '#percent_base,#percent_final,#percent', function(e) {
@@ -82,20 +82,107 @@ $(document).on('input', '#percent_base,#percent_final,#percent', function(e) {
     }
 });
 $('body').on('click', '.addmore', function(event) {
-	var newField = $('#averageFields div.averageField:last').clone();
-	var countr = $('.avg_qty').length + 1;
-	newField.find('.avg_buy_price').attr('placeholder','Buy Price '+countr);
-	newField.find('.avg_qty').attr('placeholder','Stock Quantity '+countr);
-	if(!newField.find('.avg_close').length){
-		newField.prepend('<div style="height: 25px;width: 100%;"><div class="avg_close">[<i class="fa-solid fa-xmark"></i>]</button></div>');
-	}
-	$('#averageFields').append(newField);
+    var newField = $('#averageFields div.averageField:last').clone();
+    var countr = $('.avg_qty').length + 1;
+    newField.find('.avg_buy_price').attr('placeholder', 'Buy Price ' + countr).val('');
+    newField.find('.avg_qty').attr('placeholder', 'Stock Quantity ' + countr).val('');
+    if (!newField.find('.avg_close').length) {
+        newField.prepend('<div style="height: 25px;width: 100%;"><div class="avg_close">[<i class="fa-solid fa-xmark"></i>]</div></div>');
+    }
+    $('#averageFields').append(newField);
+});
+$('body').on('click', '#StockPriceDisplay', function(event) {
+    $(this).hide();
+    $('#CurrentStockPrice').show();
+});
+$('body').on('click', '#closecurrentprice', function(event) {
+    $('#StockPriceDisplay,#CurrentStockPrice').toggle();
 });
 $('body').on('click', '.avg_close', function(event) {
     $(this).closest('.averageField').remove();
+    var i = 1;
+    $('.avg_buy_price').each(function() {
+        $(this).attr('placeholder', 'Buy Price ' + i++);
+    });
+    var j = 1;
+    $('.avg_qty').each(function() {
+        $(this).attr('placeholder', 'Stock Quantity ' + j++);
+    });
 });
 $('body').on('click', '#averageCalculatorSubmit', function(event) {
-
+    $('.averageCalculator input:visible').each(function() {
+        if (!parseInt($(this).val())) {
+            $(this).css('border-color', 'red');
+            // var error = true;
+        } else {
+            $(this).css('border-color', 'black');
+        }
+    });
+    $('.averageCalculator input:visible').each(function() {
+        if (!parseInt($(this).val())) e.preventDefault();
+    });
+    let totalinvestment = 0;
+    var totalqty = 0;
+    for (var i = 0; i < $('.averageField').length; i++) {
+        let buyprice = $('.avg_buy_price')[i].value;
+        let buyqty = $('.avg_qty')[i].value;
+        totalinvestment += buyprice * buyqty;
+        totalqty += parseInt(buyqty);
+    }
+    // console.log(totalqty);
+    let net = totalinvestment / totalqty;
+    if ($('#currentPrice:visible').length) {
+        $('.averageResult .progress,.averageResult .mb-4').show();
+        let cmp = parseInt($('#currentPrice').val()) || 0;
+        let profit = 0;
+        let profit_per = 0;
+        let currentValue = cmp * totalqty;
+        profit = currentValue - (totalinvestment);
+        profit_per = profit / totalinvestment * 100;
+        profit = Math.abs(profit).toLocaleString('en-IN', {
+            maximumFractionDigits: 0,
+            style: 'currency',
+            currency: 'INR'
+        });
+        currentValue = currentValue.toLocaleString('en-IN', {
+            maximumFractionDigits: 0,
+            style: 'currency',
+            currency: 'INR'
+        });
+        if (cmp < net) {
+            $('.averageResult #percentchange').html(profit_per.toFixed(2) + ' %').css('color', '#b70c0c');
+            // $('.averageResult #netstat').html('Net Loss : <span class="net">' + profit + '</span>').css('color', '#b70c0c');
+            $('.averageResult .progress').css('background-color', '#b70c0c');
+            $('.averageResult #cp').css('color', '#b70c0c');
+        } else {
+            $('.averageResult #percentchange').html(profit_per.toFixed(2) + ' %').css('color', '#076a07');
+            // $('.averageResult #netstat').html('Net Profit : <span class="net">' + profit + '</span>').css('color', '#076a07');
+            $('.averageResult .progress').css('background-color', '#076a07');
+            $('.averageResult #cp').css('color', 'rgb(0 189 0)');
+        }
+        $('.averageResult .progress-bar').css('width', (100 - Math.abs(profit_per)) + '%');
+        $('.averageResult #cp').html(cmp.toLocaleString('en-IN', {
+            maximumFractionDigits: 1,
+            style: 'currency',
+            currency: 'INR'
+        }));
+    } else {
+        $('.averageResult .progress,.averageResult .mb-4').hide();
+    }
+    net = net.toLocaleString('en-IN', {
+        maximumFractionDigits: 1,
+        style: 'currency',
+        currency: 'INR'
+    });
+    totalinvestment = totalinvestment.toLocaleString('en-IN', {
+        maximumFractionDigits: 1,
+        style: 'currency',
+        currency: 'INR'
+    });
+    $('.averageResult #invested').html(totalinvestment);
+    $('.averageResult #curvalue').html(totalqty);
+    $('.averageResult .net').html(net);
+    $('.averageResult').show();
 });
 $('body').on('click', '#lossCalculatorSubmit', function(event) {
     let cmp = parseInt($('#cmp').val()) || 0;
@@ -111,7 +198,7 @@ $('body').on('click', '#lossCalculatorSubmit', function(event) {
     localStorage.setItem("cmp", cmp);
     localStorage.setItem("qty", qty);
     localStorage.setItem("avg", avg);
-    $(".inputForm").hide();
+    $(".inputForm,.otherbuttons").hide();
     $(".goback,.result").show();
     $(".result:first").attr('style', 'display: flex !important;justify-content: center;margin-top: 10px;');
     $(".nav").attr('style', 'display: flex !important;');
